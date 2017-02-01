8eca34c9212ef8b332d5c5aa6ba8b5399158b886
Adrian McEwen <adrianm@mcqn.com>
2017-02-01 23:24:45 +0000

    Move init scripts over to new systemd version

--- run_logcards.sh
+++ run_logcards.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ ! -d /sys/class/gpio/gpio25 ]; then
+        echo 25 > /sys/class/gpio/export
+        echo out > /sys/class/gpio/gpio25/direction
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
